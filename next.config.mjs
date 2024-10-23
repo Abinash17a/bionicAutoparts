0772891569e400a477ad9c0634c4dfb989d32140
Abinash17a <77913801+Abinash17a@users.noreply.github.com>
2024-10-23 14:09:07 +0530

    some changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['www.shutterstock.com','images.unsplash.com','source.unsplash.com'], 
-      },
+
+ 
 };
 
 export default nextConfig;
