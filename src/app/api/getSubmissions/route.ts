f5b88801dcdaad8f8b7ee5fee95d6153f0a8b484
Abinash17a <77913801+Abinash17a@users.noreply.github.com>
2024-10-26 21:54:18 +0530

    get api chanegs

--- src/app/api/getSubmissions/route.ts
+++ src/app/api/getSubmissions/route.ts
@@ -1,30 +1,25 @@
 import { NextResponse } from 'next/server';
-import { collection, getDocs, query, orderBy, getDocsFromServer } from 'firebase/firestore';
+import { collection, getDocs, query, orderBy } from 'firebase/firestore';
 import { db } from '../../lib/firebase';
+
+export const revalidate = 1; // Revalidate API every 1 seconds
 
 export async function GET() {
   console.log("GET request received");
+
   try {
     const submissionsRef = collection(db, 'submissions');
     const q = query(submissionsRef, orderBy('createdAt', 'desc'));
 
-    const querySnapshot = await getDocsFromServer(q);
+    const querySnapshot = await getDocs(q);
 
     const submissions = querySnapshot.docs.map(doc => ({
       id: doc.id,
       ...doc.data(),
     }));
     console.log("Fetched submissions in route:", submissions);
-    const response = NextResponse.json({ submissions });
 
-
-    response.headers.set('Cache-Control', 'no-store, max-age=0');
-    response.headers.set('Access-Control-Allow-Origin', '*');
-    response.headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    response.headers.set('Access-Control-Allow-Headers', 'Content-Type');
-    response.headers.set('Access-Control-Allow-Credentials', 'true');
-
-    return response;
+    return NextResponse.json({ submissions });
   } catch (error) {
     console.error('Error fetching submissions:', error);
     return NextResponse.json(
