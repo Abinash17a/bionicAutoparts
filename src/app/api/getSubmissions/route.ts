897aea832c36ac4a713d65f52c574bfec19b5718
Abinash17a <abinashchhetri.44@outlook.com>
2024-12-26 20:45:48 +0530

    checkout page and gtag

--- src/app/api/getSubmissions/route.ts
+++ src/app/api/getSubmissions/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { collection, getDocs, query, where, orderBy } from 'firebase/firestore';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '../../lib/firebase';
 
 export const revalidate = 1; // Revalidate API every 1 second
